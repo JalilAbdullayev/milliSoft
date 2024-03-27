0b22168d694f652bd29ed85c0afd530797ddc5ac
Jalil <abdullacelil00@gmail.com>
2024-03-27 14:04:15 +0400

    Fixed Task 11

--- js/task2/task11.js
+++ js/task2/task11.js
@@ -3,12 +3,16 @@ var b = [1, 2, 4, 5]
 
 
 function compareArrays(a, b) {
+    var count = 0;
     if(a.length !== b.length) {
         return "Element sayları fərqlidir."
     }
     for(let i = 0; i < a.length; i++) {
-        if(a[i] !== b[i]) {
-            return "Elementlər fərqlidir."
+        if(a.sort()[i] == b.sort()[i]) {
+            count++
+        }
+        if(count == a.length) {
+            return "Array-lər bərabərdir."
         }
     }
     return "Array-lər bərabərdir."
