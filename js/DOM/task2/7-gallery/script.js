10d85027ee8163bae51b6735fcf8bc7767855097
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-13 06:38:40 +0400

    Update: add slideshow functionality

--- js/DOM/task2/7-gallery/script.js
+++ js/DOM/task2/7-gallery/script.js
@@ -2,38 +2,32 @@ let headerImg = document.querySelector('#header-img img');
 let galleryImg = document.getElementsByClassName('gallery-img');
 let next = document.querySelector('#next');
 let previous = document.querySelector('#previous');
+let currentImage = 0;
+
+function changeImage() {
+    currentImage = (currentImage + 1) % galleryImg.length;
+    headerImg.src = galleryImg[currentImage].children[0].src;
+}
+
+let interval = setInterval(changeImage, 3000);
 
 for(let i = 0; i < galleryImg.length; i++) {
     galleryImg[i].addEventListener('click', () => {
+        currentImage = i;
         headerImg.src = galleryImg[i].children[0].src;
-    })
-
-    document.addEventListener('DOMContentLoaded', () => {
-        headerImg.src = galleryImg[0].children[0].src;
+        clearInterval(interval);
     })
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    headerImg.src = galleryImg[0].children[0].src;
+})
+
 next.addEventListener('click', () => {
-    if(headerImg.src === galleryImg[5].children[0].src) {
-        headerImg.src = galleryImg[0].children[0].src;
-    } else {
-        for(let i = 0; i < galleryImg.length; i++) {
-            if(headerImg.src === galleryImg[i].children[0].src) {
-                headerImg.src = galleryImg[i + 1].children[0].src;
-                break;
-            }
-        }
-    }
+    changeImage();
 })
 
 previous.addEventListener('click', () => {
-    if(headerImg.src === galleryImg[0].children[0].src) {
-        headerImg.src = galleryImg[5].children[0].src;
-    } else {
-        for(let i = 0; i < galleryImg.length; i++) {
-            if(headerImg.src === galleryImg[i].children[0].src) {
-                headerImg.src = galleryImg[i - 1].children[0].src;
-            }
-        }
-    }
+    currentImage = (currentImage - 1 + galleryImg.length) % galleryImg.length;
+    headerImg.src = galleryImg[currentImage].children[0].src;
 })
