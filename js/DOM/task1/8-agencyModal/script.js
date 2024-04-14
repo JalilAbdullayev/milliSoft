3e770c2406cdd99ff06bb1da28c13c413651042d
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-14 14:15:35 +0400

    Fix: Removed repeated code

--- js/DOM/task1/8-agencyModal/script.js
+++ js/DOM/task1/8-agencyModal/script.js
@@ -1,66 +1,28 @@
-let modal1Btn = document.getElementById("modal1Btn");
-let modal2Btn = document.getElementById("modal2Btn");
-let modal3Btn = document.getElementById("modal3Btn");
-let modal4Btn = document.getElementById("modal4Btn");
-let modal5Btn = document.getElementById("modal5Btn");
-let modal6Btn = document.getElementById("modal6Btn");
+const project = document.querySelectorAll('.w-30');
+const modal = document.getElementById('modal');
 
-let modal1 = document.getElementById("modal1");
-let modal2 = document.getElementById("modal2");
-let modal3 = document.getElementById("modal3");
-let modal4 = document.getElementById("modal4");
-let modal5 = document.getElementById("modal5");
-let modal6 = document.getElementById("modal6");
+project.forEach(project => project.addEventListener("click", openModal));
 
-let body = document.querySelector("body");
-modal1Btn.addEventListener("click", () => {
-    modal1.style.display = "flex";
-    body.style.overflow = "hidden";
-});
+function openModal(event) {
+    const element = event.target.closest(".w-30");
 
-modal2Btn.addEventListener("click", () => {
-    modal2.style.display = "flex";
-    body.style.overflow = "hidden";
-});
+    if(element) {
+        const image = element.querySelector("img").src;
+        const client = element.querySelector("h4").textContent;
+        const category = element.querySelector("h5").textContent;
 
-modal3Btn.addEventListener("click", () => {
-    modal3.style.display = "flex";
-    body.style.overflow = "hidden";
-});
+        document.getElementById("image").src = image;
+        document.getElementById("client").textContent = client;
+        document.getElementById("category").textContent = category;
 
-modal4Btn.addEventListener("click", () => {
-    modal4.style.display = "flex";
-    body.style.overflow = "hidden";
-});
-
-modal5Btn.addEventListener("click", () => {
-    modal5.style.display = "flex"
-    body.style.overflow = "hidden";
-});
-
-modal6Btn.addEventListener("click", () => {
-    modal6.style.display = "flex";
-    body.style.overflow = "hidden";
-})
+        modal.style.display = "flex";
+        document.body.style.overflow = "hidden";
+    }
+}
 
 window.onclick = function(event) {
-    if(event.target == modal1) {
-        modal1.style.display = "none";
-        body.style.overflow = "visible";
-    } else if(event.target == modal2) {
-        modal2.style.display = "none";
-        body.style.overflow = "visible";
-    } else if(event.target == modal3) {
-        modal3.style.display = "none";
-        body.style.overflow = "visible";
-    } else if(event.target == modal4) {
-        modal4.style.display = "none";
-        body.style.overflow = "visible";
-    } else if(event.target == modal5) {
-        modal5.style.display = "none";
-        body.style.overflow = "visible";
-    } else if(event.target == modal6) {
-        modal6.style.display = "none";
-        body.style.overflow = "visible";
+    if(event.target === modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "visible";
     }
 }
