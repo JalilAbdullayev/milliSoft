585dae7da71500d31c8e939fbe0b949e97966e2f
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-15 18:06:26 +0400

    Moved HTML elements to JavaScript

--- js/DOM/task2/5-filter/script.js
+++ js/DOM/task2/5-filter/script.js
@@ -1,25 +1,110 @@
-let input = document.querySelectorAll('input');
 let cards = document.querySelector('#cards');
-let card = document.querySelectorAll('.card');
 let all = document.querySelector('#all');
 
-function filter() {
-    card.forEach((card) => {
-        if(card.classList.contains(this.id)) {
-            card.style.display = 'flex';
-            cards.style.gap = '105px';
-        } else {
-            card.style.display = 'none';
-        }
+let filters = ['nature', 'car', 'people'];
+let elements = [{
+    name: 'Nature 1',
+    img: 'image/nature1.jpg',
+    text: 'Lorem ipsum dolor...',
+    category: filters[0]
+}, {
+    name: 'Nature 2',
+    img: 'image/nature2.jpg',
+    text: 'Lorem ipsum dolor...',
+    category: filters[0]
+}, {
+    name: 'Nature 3',
+    img: 'image/nature3.jpg',
+    text: 'Lorem ipsum dolor...',
+    category: filters[0]
+}, {
+    name: 'Car 1',
+    img: 'image/car1.webp',
+    text: 'Lorem ipsum dolor...',
+    category: filters[1]
+}, {
+    name: 'Car 2',
+    img: 'image/car2.webp',
+    text: 'Lorem ipsum dolor...',
+    category: filters[1]
+}, {
+    name: 'Car 3',
+    img: 'image/car3.webp',
+    text: 'Lorem ipsum dolor...',
+    category: filters[1]
+}, {
+    name: 'People 1',
+    img: 'image/people1.jpg',
+    text: 'Lorem ipsum dolor...',
+    category: filters[2]
+}, {
+    name: 'People 2',
+    img: 'image/people2.jpg',
+    text: 'Lorem ipsum dolor...',
+    category: filters[2]
+}, {
+    name: 'People 3',
+    img: 'image/people3.jpg',
+    text: 'Lorem ipsum dolor...',
+    category: filters[2]
+}]
+
+function createFilter() {
+    filters.forEach((filter) => {
+        document.querySelector('#filter').innerHTML += `
+        <input type="radio" name="filter" id="${filter}"/>
+        <label for="${filter}">
+            ${filter}
+        </label>
+        `
     });
 }
 
-input.forEach((input) => {
-    input.addEventListener('click', filter);
-})
+all.setAttribute('checked', '')
+createFilter()
 
-all.addEventListener('click', () => {
-    card.forEach((card) => {
-        card.style.display = 'flex';
+function createCard() {
+    elements.forEach((element) => {
+        cards.innerHTML += `
+        <div class="card ${element.category}">
+            <div class="card-img">
+                <img src="${element.img}" alt=""/>
+                <h3>
+                    ${element.name}
+                </h3>
+                <p>
+                    ${element.text}
+                </p>
+            </div>
+        </div>
+        `
     });
-});
+}
+
+createCard()
+
+function filter() {
+    cards.innerHTML = '';
+    let value = this.getAttribute('id');
+    let result = elements.filter((element) => element.category === value);
+    result.forEach((element) => {
+        cards.innerHTML += `
+        <div class="card ${element.category}">
+            <div class="card-img">
+                <img src="${element.img}" alt=""/>
+                <h3>
+                    ${element.name}
+                </h3>
+                <p>
+                    ${element.text}
+                </p>
+            </div>
+        </div>
+        `
+    });
+    if(this.id === 'all') {
+        createCard();
+    }
+}
+
+document.querySelectorAll('input').forEach((el) => el.addEventListener('click', filter));
