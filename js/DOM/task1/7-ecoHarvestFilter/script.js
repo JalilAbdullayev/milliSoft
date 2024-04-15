1999b9805f6141af0aea303b37888467edd601c0
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-15 17:14:34 +0400

    Removed repeated code

--- js/DOM/task1/7-ecoHarvestFilter/script.js
+++ js/DOM/task1/7-ecoHarvestFilter/script.js
@@ -8,148 +8,48 @@ let vegetables = document.getElementById('vegetables');
 let driedFruits = document.getElementById('driedFruits');
 let driedVegetables = document.getElementById('driedVegetables');
 
-filter_fruit.style.color = '#408858';
+const products = [fruits, vegetables, driedFruits, driedVegetables];
+const filters = [filter_fruit, filter_vegetable, filter_driedFruit, filter_driedVegetable];
 
 let style = document.createElement('style');
 document.head.appendChild(style);
 
-style.appendChild(document.createTextNode(`#${filter_fruit.id}::after {
+function filterProduct(filter, product) {
+    const productArr = products.filter(p => p !== product);
+    const filterArr = filters.filter(f => f !== filter);
+    filterArr.forEach(f => f.style.color = '#7B8E9D');
+    filter.style.color = '#408858';
+    let styles = "";
+    filterArr.forEach(f => {
+        if(f !== event.target) {
+            styles += `#${f.id}::after {
+            opacity: 0;
+            }`;
+        }
+    });
+
+    style.appendChild(document.createTextNode(`#${filter.id}::after {
             opacity: 1;
         }`));
-style.appendChild(document.createTextNode(`#${filter_driedFruit.id}::after {
-            opacity: 0;
-        }`));
-style.appendChild(document.createTextNode(`#${filter_driedVegetable.id}::after {
-            opacity: 0;
-        }`));
-style.appendChild(document.createTextNode(`#${filter_vegetable.id}::after {
-            opacity: 0;
-        }`));
 
-filter_fruit.addEventListener('click', () => {
-    filter_fruit.style.color = '#408858';
-    filter_driedFruit.style.color = '#7B8E9D';
-    filter_driedVegetable.style.color = '#7B8E9D';
-    filter_vegetable.style.color = '#7B8E9D';
+    style.appendChild(document.createTextNode(styles));
+    productArr.forEach(p => p.style.opacity = '0');
 
-    style.appendChild(document.createTextNode(`#${filter_fruit.id}::after {
-            opacity: 1;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_driedFruit.id}::after {
-            opacity: 0;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_driedVegetable.id}::after {
-            opacity: 0;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_vegetable.id}::after {
-            opacity: 0;
-        }`));
-
-    fruits.style.opacity = '1';
-    vegetables.style.opacity = '0';
-    driedFruits.style.opacity = '0';
-    driedVegetables.style.opacity = '0';
+    product.style.opacity = '1';
 
     setTimeout(() => {
-        fruits.style.display = 'flex';
-        vegetables.style.display = 'none';
-        driedFruits.style.display = 'none';
-        driedVegetables.style.display = 'none';
+        productArr.forEach(p => p.style.display = 'none');
+        product.style.display = 'flex';
     }, 300);
-});
+}
 
-filter_vegetable.addEventListener('click', () => {
-    filter_fruit.style.color = '#7B8E9D';
-    filter_vegetable.style.color = '#408858';
-    filter_driedFruit.style.color = '#7B8E9D';
-    filter_driedVegetable.style.color = '#7B8E9D';
+document.addEventListener('DOMContentLoaded', () => filterProduct(filter_fruit, fruits));
 
-    style.appendChild(document.createTextNode(`#${filter_fruit.id}::after {
-            opacity: 0;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_vegetable.id}::after {
-            opacity: 1;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_driedFruit.id}::after {
-            opacity: 0;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_driedVegetable.id}::after {
-            opacity: 0;
-        }`));
 
-    fruits.style.opacity = '0';
-    vegetables.style.opacity = '1';
-    driedFruits.style.opacity = '0';
-    driedVegetables.style.opacity = '0';
+filter_fruit.addEventListener('click', () => filterProduct(filter_fruit, fruits));
 
-    setTimeout(() => {
-        fruits.style.display = 'none';
-        vegetables.style.display = 'flex';
-        driedFruits.style.display = 'none';
-        driedVegetables.style.display = 'none';
-    }, 300);
-});
+filter_vegetable.addEventListener('click', () => filterProduct(filter_vegetable, vegetables));
 
-filter_driedFruit.addEventListener('click', () => {
-    filter_fruit.style.color = '#7B8E9D';
-    filter_vegetable.style.color = '#7B8E9D';
-    filter_driedFruit.style.color = '#408858';
-    filter_driedVegetable.style.color = '#7B8E9D';
+filter_driedFruit.addEventListener('click', () => filterProduct(filter_driedFruit, driedFruits));
 
-    style.appendChild(document.createTextNode(`#${filter_fruit.id}::after {
-            opacity: 0;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_vegetable.id}::after {
-            opacity: 0;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_driedFruit.id}::after {
-            opacity: 1;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_driedVegetable.id}::after {
-            opacity: 0;
-        }`));
-
-    fruits.style.opacity = '0';
-    vegetables.style.opacity = '0';
-    driedFruits.style.opacity = '1';
-    driedVegetables.style.opacity = '0';
-
-    setTimeout(() => {
-        fruits.style.display = 'none';
-        vegetables.style.display = 'none';
-        driedFruits.style.display = 'flex';
-        driedVegetables.style.display = 'none';
-    }, 300);
-});
-
-filter_driedVegetable.addEventListener('click', () => {
-    filter_fruit.style.color = '#7B8E9D';
-    filter_vegetable.style.color = '#7B8E9D';
-    filter_driedFruit.style.color = '#7B8E9D';
-    filter_driedVegetable.style.color = '#408858';
-
-    style.appendChild(document.createTextNode(`#${filter_fruit.id}::after {
-            opacity: 0;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_vegetable.id}::after {
-            opacity: 0;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_driedFruit.id}::after {
-            opacity: 0;
-        }`));
-    style.appendChild(document.createTextNode(`#${filter_driedVegetable.id}::after {
-            opacity: 1;
-        }`));
-
-    fruits.style.opacity = '0';
-    vegetables.style.opacity = '0';
-    driedFruits.style.opacity = '0';
-    driedVegetables.style.opacity = '1';
-
-    setTimeout(() => {
-        fruits.style.display = 'none';
-        vegetables.style.display = 'none';
-        driedFruits.style.display = 'none';
-        driedVegetables.style.display = 'flex';
-    }, 300);
-});
+filter_driedVegetable.addEventListener('click', () => filterProduct(filter_driedVegetable, driedVegetables));
