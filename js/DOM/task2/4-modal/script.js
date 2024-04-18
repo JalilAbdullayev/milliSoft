b8a4ed004ca76938754c7961816493b6a221660f
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-18 18:19:51 +0400

    Fix: moved HTML to JS

--- js/DOM/task2/4-modal/script.js
+++ js/DOM/task2/4-modal/script.js
@@ -1,39 +1,56 @@
-let images = document.querySelectorAll(".image");
-let imagesImg = document.querySelectorAll(".image img");
+let array = [{
+    name: 'Image 1',
+    image: 'image1.jpg'
+}, {
+    name: 'Image 2',
+    image: 'image2.webp'
+}, {
+    name: 'Image 3',
+    image: 'image3.avif'
+}, {
+    name: 'Image 4',
+    image: 'image4.avif'
+}];
 
-images.forEach((image, index) => {
-    image.addEventListener("click", () => {
-        let modalContainer = document.createElement('div');
-        modalContainer.setAttribute('id', 'modal-container');
-        document.body.appendChild(modalContainer);
-
-        modalContainer.addEventListener("click", () => {
-            modalContainer.style.display = "none";
-        })
-
-        let modal = document.createElement('div');
-        modal.setAttribute('class', 'modal');
-        modalContainer.appendChild(modal);
-
-        let modalImg = document.createElement('img');
-        modalImg.setAttribute('src', imagesImg[index].src);
-        modal.appendChild(modalImg);
-
-        let title = document.createElement('h3');
-        let titleText = document.createTextNode(imagesImg[index].alt);
-        title.appendChild(titleText);
-        modal.appendChild(title);
-
-        let close = document.createElement('span');
-        close.setAttribute('class', 'close');
-        let closeIcon = document.createTextNode("X");
-        close.appendChild(closeIcon);
-        modal.appendChild(close);
-
-        close.addEventListener('click', () => {
-            modalContainer.style.display = "none";
-        })
-
-        modalContainer.style.display = "flex";
-    });
+array.forEach((element, index) => {
+    let image = document.createElement('img');
+    image.setAttribute('src', 'image/' + element.image);
+    image.setAttribute('alt', element.name);
+    image.setAttribute('onclick', 'showModal(' + index + ')');
+    document.getElementById('container').appendChild(image);
 });
+
+function showModal(i) {
+    let modalContainer = document.createElement('div');
+    modalContainer.setAttribute('id', 'modal-container');
+    document.body.appendChild(modalContainer);
+
+    modalContainer.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    })
+
+    let modal = document.createElement('div');
+    modal.setAttribute('class', 'modal');
+    modalContainer.appendChild(modal);
+
+    let modalImg = document.createElement('img');
+    modalImg.setAttribute('src', 'image/' + array[i].image);
+    modal.appendChild(modalImg);
+
+    let title = document.createElement('h3');
+    let titleText = document.createTextNode(array[i].name);
+    title.appendChild(titleText);
+    modal.appendChild(title);
+
+    let close = document.createElement('span');
+    close.setAttribute('class', 'close');
+    let closeIcon = document.createTextNode("X");
+    close.appendChild(closeIcon);
+    modal.appendChild(close);
+
+    close.addEventListener('click', () => {
+        modalContainer.style.display = "none";
+    })
+
+    modalContainer.style.display = "flex";
+}
