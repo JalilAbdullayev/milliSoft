3866db885fc569c7b3ad792da5785425f42f8077
JalilAbdullayev <abdullacelil00@gmail.com>
2024-05-21 14:12:48 +0400

    Update: Task 2

--- sql/task2.sql
+++ sql/task2.sql
@@ -1,50 +1,53 @@
 CREATE DATABASE sales
 
 CREATE TABLE filial (
-    id      INT IDENTITY PRIMARY KEY,
-    name    VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL
+    filial_id      INT IDENTITY PRIMARY KEY,
+    filial_name    VARCHAR(255) NOT NULL,
+    filial_address VARCHAR(255) NOT NULL
 )
 
 INSERT INTO filial
 VALUES ('28 May', '28 May')
 
 CREATE TABLE gender (
-    id   TINYINT IDENTITY PRIMARY KEY,
-    name NVARCHAR(5) NOT NULL
+    gender_id   TINYINT IDENTITY PRIMARY KEY,
+    gender_name NVARCHAR(5) NOT NULL
 )
 
 INSERT INTO gender
 VALUES (N'Kişi'),
        (N'Qadın')
 
-CREATE TABLE category (
-    id   INT IDENTITY PRIMARY KEY,
-    name NVARCHAR(255) NOT NULL
+CREATE TABLE categories (
+    category_id   INT IDENTITY PRIMARY KEY,
+    category_name NVARCHAR(255) NOT NULL
 )
 
-INSERT INTO category
+INSERT INTO categories
 VALUES ('Elektronika'),
        ('Telefon'),
        ('Kompyuter')
 
 CREATE TABLE marka (
-    id   INT IDENTITY PRIMARY KEY,
-    name NVARCHAR(255) NOT NULL
+    marka_id   INT IDENTITY PRIMARY KEY,
+    marka_name NVARCHAR(255) NOT NULL
 )
 
 INSERT INTO marka
 VALUES ('Apple'),
        ('Samsung'),
-       ('Xiaomi')
+       ('Xiaomi'),
+       ('Lenovo'),
+       ('HP'),
+       ('Dell')
 
-CREATE TABLE model (
-    id       INT IDENTITY PRIMARY KEY,
-    name     NVARCHAR(255) NOT NULL,
-    marka_id INT FOREIGN KEY REFERENCES marka (id)
+CREATE TABLE models (
+    model_id       INT IDENTITY PRIMARY KEY,
+    model_name     NVARCHAR(255) NOT NULL,
+    model_marka_id INT FOREIGN KEY REFERENCES marka(marka_id)
 )
 
-INSERT INTO model
+INSERT INTO models
 VALUES ('iPhone 13', 1),
        ('iPhone 14', 1),
        ('iPhone 15', 1),
@@ -53,57 +56,132 @@ VALUES ('iPhone 13', 1),
        ('Galaxy S23', 2),
        ('Xiaomi 13', 3),
        ('Xiaomi 14', 3),
-       ('Xiaomi 15', 3)
+       ('Xiaomi 15', 3),
+       ('Lenovo Legion', 4),
+       ('HP Omen', 5),
+       ('Dell Alienware', 6)
 
-CREATE TABLE client (
-    id           INT IDENTITY PRIMARY KEY,
-    name         NVARCHAR(20)  NOT NULL,
-    surname      NVARCHAR(20)  NOT NULL,
-    father_name  NVARCHAR(20)  NOT NULL,
-    FIN          VARCHAR(7)    NOT NULL,
-    birthdate    DATE          NOT NULL,
-    gender_id    TINYINT FOREIGN KEY REFERENCES gender (id),
-    phone_number VARCHAR(10)   NOT NULL,
-    address      NVARCHAR(255) NOT NULL
+--13.
+SELECT CONCAT(model_name, ' (', LEN(model_name), ')')
+FROM models
+
+CREATE TABLE clients (
+    client_id           INT IDENTITY PRIMARY KEY,
+    client_name         NVARCHAR(20)  NOT NULL,
+    client_surname      NVARCHAR(20)  NOT NULL,
+    client_father_name  NVARCHAR(20)  NOT NULL,
+    client_FIN          VARCHAR(7)    NOT NULL,
+    client_birthdate    DATE          NOT NULL,
+    client_gender_id    TINYINT FOREIGN KEY REFERENCES gender(gender_id),
+    client_phone_number VARCHAR(10)   NOT NULL,
+    client_address      NVARCHAR(255) NOT NULL
 )
 
-INSERT INTO client
+INSERT INTO clients
 VALUES (N'Əli', N'Əliyev', N'Əliqulu', '1Y215T1', '2000-01-01', 1, '0551548954', 'Baku')
 
-CREATE TABLE seller (
-    id          INT IDENTITY PRIMARY KEY,
-    name        NVARCHAR(20) NOT NULL,
-    surname     NVARCHAR(20) NOT NULL,
-    father_name NVARCHAR(20) NOT NULL,
-    birthdate   DATE         NOT NULL,
-    gender_id   TINYINT FOREIGN KEY REFERENCES gender (id),
-    salary      SMALLINT     NOT NULL,
-    filial_id   INT FOREIGN KEY REFERENCES filial (id)
+CREATE TABLE sellers (
+    seller_id          INT IDENTITY PRIMARY KEY,
+    seller_name        NVARCHAR(20) NOT NULL,
+    seller_surname     NVARCHAR(20) NOT NULL,
+    seller_father_name NVARCHAR(20) NOT NULL,
+    seller_birthdate   DATE         NOT NULL,
+    seller_gender_id   TINYINT FOREIGN KEY REFERENCES gender(gender_id),
+    seller_salary      SMALLINT     NOT NULL,
+    seller_filial_id   INT FOREIGN KEY REFERENCES filial(filial_id),
+    seller_age         TINYINT      NOT NULL
 )
 
-INSERT INTO seller
-VALUES (N'Jalə', N'Ələsgərova', N'Sabir', '2000-01-01', 2, 1000, 1)
+INSERT INTO sellers
+VALUES (N'Jalə', N'Ələsgərova', N'Sabir', '2000-01-01', 2, 1000, 1, 24),
+       ('Murad', N'Ələkbərov', 'Rasim', '2001-02-04', 1, 1200, 1, 23)
 
-CREATE TABLE product (
-    id             INT IDENTITY PRIMARY KEY,
-    category_id    INT FOREIGN KEY REFERENCES category (id),
-    model_id       INT FOREIGN KEY REFERENCES model (id),
-    purchase_price INT NOT NULL,
-    sale_price     INT NOT NULL
+--2.
+SELECT *
+FROM sellers;
+
+--4.
+SELECT *
+FROM sellers
+WHERE seller_name = 'Murad'
+
+--5.
+SELECT *
+FROM sellers
+WHERE seller_age < 25
+
+--12.
+SELECT CONCAT(seller_name, ' ', seller_surname, ' ', seller_father_name) 'Name Surname Father name'
+FROM sellers
+
+CREATE TABLE products (
+    product_id             INT IDENTITY PRIMARY KEY,
+    product_category_id    INT FOREIGN KEY REFERENCES categories(category_id),
+    product_model_id       INT FOREIGN KEY REFERENCES models(model_id),
+    product_purchase_price INT NOT NULL,
+    product_sale_price     INT NOT NULL,
+    product_quantity       INT NOT NULL DEFAULT 0
 )
 
-INSERT INTO product
-VALUES (2, 1, 1000, 2000)
+INSERT INTO products
+VALUES (2, 1, 1000, 2000, 2),
+       (2, 2, 2000, 3000, 1),
+       (3, 10, 10000, 20000, 3),
+       (3, 11, 20000, 30000, 4),
+       (3, 12, 30000, 40000, 2)
 
-CREATE TABLE sale (
-    id         INT IDENTITY PRIMARY KEY,
-    client_id  INT FOREIGN KEY REFERENCES client (id),
-    seller_id  INT FOREIGN KEY REFERENCES seller (id),
-    product_id INT FOREIGN KEY REFERENCES product (id),
-    quantity   INT  NOT NULL,
-    discount   TINYINT,
-    date       DATE NOT NULL
+--1.
+SELECT *
+FROM products
+
+--3.
+SELECT product_id, product_category_id, product_model_id, product_purchase_price, product_sale_price, category_name
+FROM products
+         INNER JOIN categories ON categories.category_id = products.product_category_id
+
+--6.
+SELECT product_model_id, COUNT(product_model_id) 'model_count'
+FROM products
+GROUP BY product_model_id
+
+--7.
+SELECT model_marka_id 'marka_id', model_id, SUM(product_quantity) 'product_count'
+FROM models
+         INNER JOIN products ON models.model_id = products.product_model_id
+GROUP BY model_marka_id, model_id
+
+--14.
+SELECT TOP 1 *
+FROM products
+ORDER BY product_sale_price DESC
+
+CREATE TABLE sales (
+    sale_id         INT IDENTITY PRIMARY KEY,
+    sale_client_id  INT FOREIGN KEY REFERENCES clients(client_id),
+    sale_seller_id  INT FOREIGN KEY REFERENCES sellers(seller_id),
+    sale_product_id INT FOREIGN KEY REFERENCES products(product_id),
+    sale_quantity   INT      NOT NULL,
+    sale_discount   TINYINT,
+    sale_datetime   DATETIME NOT NULL
 )
 
-INSERT INTO sale
-VALUES (1, 1, 1, 1, 10, '2022-01-01')
+INSERT INTO sales
+VALUES (1, 1, 1, 1, 10, '2022-01-01 11:42'),
+       (1, 2, 3, 3, 0, '2024-01-30 17:50'),
+       (1, 2, 4, 1, 15, '2024-01-30 17:50'),
+       (1, 1, 5, 2, 5, '2024-02-01 17:48'),
+       (1, 1, 2, 1, 1, '2024-03-09 16:37'),
+       (1, 1, 5, 4, 0, '2024-04-07 14:53')
+
+--9.
+SELECT TOP 1 COUNT(sale_product_id) 'sale_count', SUM(sale_quantity) 'quantity', product_model_id
+FROM sales
+         JOIN products p ON sales.sale_product_id = p.product_id
+GROUP BY product_model_id
+ORDER BY sale_count DESC, quantity DESC
+
+--10.
+SELECT TOP 1 sale_seller_id, COUNT(sale_id) 'sale_count'
+FROM sales
+GROUP BY sale_seller_id
+ORDER BY sale_count
